4314bcd355a09737e481ffedc61c5ef29b3475cb
Mark Howe <mvahowe@gmail.com>
2025-07-11 13:02:25 +0200

    Add timeout

--- src/AudioRecorder.jsx
+++ src/AudioRecorder.jsx
@@ -14,14 +14,16 @@ const AudioRecorder = () => {
             return;
         }
         try {
-            console.log("Starting recording");
+            console.log("Starting recording, maybe");
             
             const stream = await navigator.mediaDevices.getUserMedia(
                 { audio: true }
             );
+            console.log("We have a stream");
             mediaStream.current = stream;
             mediaRecorder.current = new MediaRecorder(stream);
             mediaRecorder.current.ondataavailable = (e) => {
+                console.log("data available")
                 if (e.data.size > 0) {
                     chunks.current.push(e.data);
                 }
@@ -29,7 +31,7 @@ const AudioRecorder = () => {
             mediaRecorder.current.onstop = () => {
                 console.log("Event: Recording stopped");
             };
-            mediaRecorder.current.start();
+            mediaRecorder.current.start(1000);
         } catch (error) {
             console.error('Error accessing microphone:', error);
         }
